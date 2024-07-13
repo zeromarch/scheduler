736c936de1df612abd29eaf394546358ff1ef23a
Marcello Tommasi <marcellotommasi@Marcellos-MacBook-Pro.local>
2024-07-13 15:45:50 -0400

    Integrate front-end with Flask API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from models import db, Availability
 from routes import availability_bp
 from config import Config
@@ -17,6 +17,10 @@ create_tables()
 
 app.register_blueprint(availability_bp, url_prefix='/api')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
